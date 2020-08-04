be5f3b56eb12ed3eeccc7bd478b130872cdb93ed
dzakiahsana <dzakiahsana@gmail.com>
2020-08-04 08:03:43 +0700

    Reactjs Components & Props

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Table from './tugas11/Table';
+
+function App() {
+  return (
+    <div>
+      {/* tugas 11*/}
+       <Table /> 
+    </div>
+  );
+}
+
+export default App;
